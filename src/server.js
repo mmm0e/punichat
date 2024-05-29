58b739a916ed5c4784ba6ea68710a9d5f8dc88d0
mmm0e <87794639+mmm0e@users.noreply.github.com>
2024-05-29 15:58:37 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const http = require('http');
-
-// Socket.ioをインポート
 const socketIo = require('socket.io');
 
 const app = express();
 const server = http.Server(app);
-
-// 初期化 サーバーでSocket.IOを使える状態にする
 const io = socketIo(server);
 
 const PORT = 3000;
+
+// express.staticを使用して静的ファイルを提供
+app.use(express.static(__dirname));
 
 // ルーティングの設定。'/' にリクエストがあった場合 src/index.html を返す
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// 3000番ポートでHTTPサーバーを起動
+// HTTPサーバーを起動
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
@@ -27,14 +26,11 @@ server.listen(PORT, () => {
 io.on('connection', (socket) => {
   console.log('connected');
 
-  // 'pointermove' というイベント名で受信できる
-  // 第一引数には受信した座標が入り、ログに出力する
-  socket.on('pointermove', (id, x, y) => {
-    console.log(id, x, y);
-    
-    // 'bunnyreceive' というイベントを発火
-    // 受信した座標を全てのクライアントに対して送信する
-    io.emit('bunnyreceive',id, x, y);
+  // 'ballsmove' というイベント名で受信
+  socket.on('ballsmove', (ballsData) => {
+    //console.log('balls: ', ballsData);
+    // 'ballsupdate' というイベントを発火
+    io.emit('ballsupdate', ballsData);
   });
 
 });
