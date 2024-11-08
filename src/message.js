d5a9728903ee97811a142a4130a223e9a1091d88
mmm0e <87794639+mmm0e@users.noreply.github.com>
2024-11-08 12:07:48 +0900

    Update message.js

--- src/message.js
+++ src/message.js
@@ -7,37 +7,38 @@ export class MessageApp extends PIXI.Sprite {
     this._w = w
     this._h = h
 
-    // this.app = app
-    // this.socket = socket
     this.clientId = clientId
     this.messages = []
-    this.messageYPosition = 100 // メッセージの初期位置
+    this.messageYPosition = 80 // メッセージの初期位置
 
     // メッセージ送信ボタンクリックイベント
-    document
-      .getElementById('sendBtn')
-      .addEventListener('click', () => this.sendMessage())
+    document.getElementById('sendBtn').addEventListener('click', () => this.sendMessage())
+    document.getElementById('inputText').addEventListener('keydown', (e) => {
+			if (e.key === 'Enter') this.sendMessage()
+    })
   }
 
   async displayMessage(message, senderId, messageId) {
+
     const container = new PIXI.Container()
     container.id = messageId || `${this.clientId}-${Date.now()}`
 
     const isSelf = senderId === this.clientId
     container.animInstance = new animation()
+
     const anim = await container.animInstance.createAnimation(isSelf)
 
     let text = new PIXI.Text(message, {
       fontFamily: 'Arial',
-      fontSize: 20,
+      fontSize: 15,
       fill: 'black',
       wordWrap: true,
       breakWords: true,
-      wordWrapWidth: 250,
+      wordWrapWidth: 200,
     })
 
     const textBounds = text.getLocalBounds()
-    anim.width = textBounds.width + 40
+    anim.width = textBounds.width + 30
     anim.height = textBounds.height + 30
     anim.x = isSelf ? this._w - (anim.width / 2 + 50) : 50 + anim.width / 2
     anim.y = this.messageYPosition
@@ -52,18 +53,16 @@ export class MessageApp extends PIXI.Sprite {
 
     container.interactive = true
     container.buttonMode = true
+
     container.on('pointerdown', () => {
       if (senderId != this.clientId) {
         container.animInstance.playAnimation()
         console.log('send animationClick event')
-        this.emit('animationClick', {
-          messageId: container.id,
-          senderId: senderId,
-        })
+        this.emit('animationClick', {messageId: container.id,senderId: senderId,})
       }
     })
 
-    this.messageYPosition += anim.height + 20
+    this.messageYPosition += anim.height + 30
     this.messages.push(container)
   }
 
@@ -96,17 +95,13 @@ export class MessageApp extends PIXI.Sprite {
 
     //コンテナの存在をチェック
     if (container) {
-      // const animationSprite = container.children.find(child => child instanceof PIXI.AnimatedSprite);
-      // if (animationSprite) {
-      //     animationSprite.play();
-      // }
       //アニメーション再生
       container.animInstance.playAnimation()
     }
   }
 
-  setSize(w, h) {
-    console.log(w, h)
-    //todo 自分でサイズ変更かいて
-  }
+//   setSize(w, h) {
+//     console.log(w, h)
+//     //todo 自分でサイズ変更かいて
+//   }
 }
