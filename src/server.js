a2cee7b23d4b24c60bab4d4f9a80f1e80029a06e
mmm0e <87794639+mmm0e@users.noreply.github.com>
2024-11-18 23:12:01 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,34 +1,37 @@
 const express = require('express')
 const http = require('http')
 const socketIo = require('socket.io')
+
 const app = express()
-const server = http.Server(app)
+const PORT = 3000
+const HOST = '0.0.0.0'  // 全てのIPアドレスからのアクセスを許可
+
+const server = http.createServer(app)
 const io = socketIo(server)
 
-const PORT = 3000
-
 // express.staticを使用して静的ファイルを提供
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
 // ルーティングの設定。'/' にリクエストがあった場合 src/index.html を返す
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index_chat.html')
-})
+  res.sendFile(__dirname + '/index_chat.html');
+});
 
 // HTTPサーバーを起動
-server.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+server.listen(PORT, HOST, () => {
+  console.log(`listening on http://${HOST}:${PORT}`);
+});
 
-// クライアントとのコネクションが確立したら'connected'という表示させる
 // サーバーへのアクセスを監視。アクセスがあったらコールバックが実行
 io.on('connection', (socket) => {
-  	console.log('connected');
-	// 受信
-	let clientId;
+  	console.log("A user connected:", socket.id);
+	
+	socket.on("disconnect", () => {
+        console.log("A user disconnected:", socket.id);
+    });
+
 	socket.on('registerClient', (id) => {
-		clientId = id;
-		console.log(`Client registered: ${clientId}`);
+		console.log(`Client registered: ${id}`);
 	});
 
 	socket.on('sendMessage', (message) => {
